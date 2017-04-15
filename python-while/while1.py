37b638bcef51f4d6cd70feaa6282fb824c0cdb6f
BenDeb <bendebsx@gmail.com>
2017-04-15 14:35:14 +0200

    boucle nombre magique

--- python-while/while1.py
+++ python-while/while1.py
@@ -1,6 +1,14 @@
-a = 0
-while a < 100:
+nombre = 0
+while nombre != 5:
+    print('cherchez le nombre magique')
+    nombre = input()
+    if int(nombre) < 5:
+        print('plus grand!')
+        continue
+    if int(nombre) > 5:
+        print('plus petit!')
+        continue
+    if int(nombre) == 5:
+        break
+print('bravo')
 
-    print('a est maintenant égal à', a)
-#a
-    a += 1
